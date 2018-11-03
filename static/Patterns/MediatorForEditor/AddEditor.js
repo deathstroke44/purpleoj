d4acc6316f7bc0baaa207fad93ac21836f03bfdb
muntakimMohiuddin <2015-816-773@student.cse.du.ac.bd>
2018-11-04 03:34:34 +0600

    Revert "Added themes for submissions and added further realtime autocompletion for code editor"
    This reverts commit ede460c8a89838221f3cd84b1b5fd95859b81179.

--- static/Patterns/MediatorForEditor/AddEditor.js
+++ static/Patterns/MediatorForEditor/AddEditor.js
@@ -5,10 +5,6 @@ class AddEditor{
                         document.getElementById(id),
                         config);
         this.mediator._editor = this.editor;
-        this.editor.on("keyup", function (cm, event) {
-            mediator.editorKeyUp(cm, event);
-        });
-
     }
 
 }
